b5a9a58fb00f57eed61afdc6106cb19030838967
Alpet Gexha <50520333+AlpetGexha@users.noreply.github.com>
2025-05-13 00:07:54 +0200

    lock in

--- config/services.php
+++ config/services.php
@@ -43,6 +43,11 @@ return [
     'openai' => [
         'key' => env('OPENAI_API_KEY'),
         'model' => env('OPENAI_MODEL', 'gpt-3.5-turbo'),
+    ],
+    
+    'inspector' => [
+        'ingestion_key' => env('INSPECTOR_INGESTION_KEY'),
+        'enabled' => env('INSPECTOR_ENABLED', false),
     ]
 
 ];
